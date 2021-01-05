3957e075ed395fe77413f44f3fda55b3df3d515e
Kajetan Kuzma <kajetankuzma@gmail.com>
2021-01-05 21:35:18 +0100

    Added  some code in script.js file  responsible for hidding menu when link is clicked.

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 document.addEventListener("DOMContentLoaded", appStart)
 function appStart()
-{
+{   
+    const navigation = document.querySelector('.navigation');
     const menu_button = document.getElementById('menu_button');
+    const menu_link = document.querySelectorAll('.menu_link');
     menu_button.addEventListener('click', toggleMenu, false);
     
+    for (let i=0; i< menu_link.length; i++)
+    {
+        menu_link[i].addEventListener('click', function(){
+            navigation.style.display = 'none';
+        }, false);
+    }
 
     function toggleMenu()
     {
-        const navigation = document.querySelector('.navigation');
+        
         if(navigation.style.display == 'initial')
         {
             navigation.style.display = 'none';
